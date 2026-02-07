73cbf416e023213c9161ab446f07c95812646350
Chris <davischris2207@gmail.com>
2026-02-07 14:40:52 +0000

    Added Native Input Gameplay Tags

---
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Christopher Davis All Rights reserved
+
+
+#include "WarriorGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
