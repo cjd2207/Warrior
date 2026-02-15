76428c83c6b58ec8c2061ac7a9ee047c7eb1258d
Chris <davischris2207@gmail.com>
2026-02-15 10:53:51 +0000

    Hero Anim Instance

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -2,4 +2,31 @@
 
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float deltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(deltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += deltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
