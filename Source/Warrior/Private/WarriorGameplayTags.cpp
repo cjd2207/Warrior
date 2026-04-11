f7e8b6c5f143e733cc2b3891c41698a01575d7fc
Chris <davischris2207@gmail.com>
2026-04-11 11:41:02 +0100

    Light Attack Ability

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -10,10 +10,14 @@ namespace WarriorGameplayTags
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_EquippedAxe, "InputTag.EquippedAxe");
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequippedAxe, "InputTag.UnequippedAxe");
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack_Axe, "InputTag.LightAttack.Axe");
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack_Axe, "InputTag.HeavyAttack.Axe");
 
 	/**/
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axe, "Player.Ability.Equip.Axe");
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(Player_Ability_UnEquip_Axe, "Player.Ability.UnEquip.Axe");
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light_Axe, "Player.Ability.Attack.Light.Axe");
+	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy_Axe, "Player.Ability.Attack.Heavy.Axe");
 
 	WARRIOR_API UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
 
